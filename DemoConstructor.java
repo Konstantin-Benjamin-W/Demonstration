d30e28d232aa84cd8e4e09f4235e6579f33fd540
Konstantin-Benjamin-W <konstantin.bwb@gmail.com>
2019-07-24 12:13:00 +0300

    Constructors

---
+++ DemoConstructor.java
@@ -0,0 +1,15 @@
+public class DemoConstructor {
+
+        private int id;
+        private String text;
+        //constructor without parameters
+        public DemoConstructor(){
+            super();
+        }
+        //constructor with parameters
+        public DemoConstructor(int idc, String txt){
+            super();
+            id = idc;
+            text = txt;
+        }
+    }
